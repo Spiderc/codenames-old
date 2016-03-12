554e817caedc46643cae78beff9478bd388ee98d
Andrew Schleicher <spiderc126@yahoo.com>
2016-03-11 18:59:07 -0600

    Added Import
    created a string that can be used to import a board

--- codenames.js
+++ codenames.js
@@ -1,4 +1,9 @@
+var hexMap = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
+
 $(document).ready(function(){
+	clearBoard();
+	$("#boardId").val("");
+	
 	$("#generateButton").click(function(){
 		clearBoard();
 		generateBoard();
@@ -38,10 +43,56 @@ function generateBoard(){
 		}
 		board.push(value);
 	}
+	createBoardId(board);
+}
+
+function importBoard(){
+	var boardId = $("#boardId").val();
+	var array = [];
+	for(var i=0;i<12;i++){
+		var value = hexMap.indexOf(boardId.charAt(i));
+		array.push(value >> 2);
+		array.push(value & ~12);
+	}
+	array.push(parseInt(boardId.charAt(12)));
+	var redCount = 0;
+	for(var i=0;i<25;i++){
+		var value = array[i];
+		if(value === 0){
+			$("#tableSection" + (i + 1)).addClass("red");
+			redCount = redCount + 1;
+		} else if(value === 1){
+			$("#tableSection" + (i + 1)).addClass("blue");
+		} else if(value === 2){
+			$("#tableSection" + (i + 1)).addClass("bystander");
+		} else if(value === 3){
+			$("#tableSection" + (i + 1)).addClass("assassin");
+		}
+	}
+	if(redCount === 9){
+		$("#startingTeam").text("Red");
+		$("#startingTeam").addClass("red");
+		$("#startingTeam").removeClass("blue");
+	} else {
+		$("#startingTeam").text("Blue");
+		$("#startingTeam").addClass("blue");
+		$("#startingTeam").removeClass("red");
+	}
+}
+
+function createBoardId(board){
+	var boardId = "";
+	for(var i=0;i<12;i++){
+		var spot1 = board[(2 * i)]
+		var spot2 = board[(2 * i) + 1]
+		boardId = boardId + hexMap[(spot1 << 2) + spot2];
+	}
+	boardId = boardId + board[24];
+	$("#boardId").val(boardId);
 }
 
 function clearBoard(){
-	for(var i=1;i<26;i++){
+	for(var i=0;i<25;i++){
 		$("#tableSection" + (i + 1)).removeClass("red");
 		$("#tableSection" + (i + 1)).removeClass("blue");
 		$("#tableSection" + (i + 1)).removeClass("bystander");
